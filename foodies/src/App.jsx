9a9fd42b3a6d32b49aecf6d3896ebc559555f090
orhan <orhantrkmn749@gmail.com>
2025-10-02 21:06:59 +0300

    pages explore isimlendirmesi rename yapıldı

--- foodies/src/App.jsx
+++ foodies/src/App.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import { Menubar } from './components/Menubar/Menubar'
+import React from "react";
+import { Menubar } from "./components/Menubar/Menubar";
+import { Route, Routes } from "react-router";
+import { Home } from "./pages/Home/Home";
+import ContactUs from "./pages/Contact Us/ContactUs";
+import { Explore } from "./pages/ExploreFood/ExploreFood";
 
 const App = () => {
   return (
-    <div><Menubar/></div>
-  )
-}
+    <div>
+      <Menubar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/explore" element={<Explore />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
