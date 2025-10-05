8eb87a22889b28b6c1d55c0574fb803ed78be48b
orhan <orhantrkmn749@gmail.com>
2025-10-05 10:40:02 +0300

    FoodItem ürün kartlarına sepete ekle butonu için miktar arttırma azaltma eklendi.

--- foodies/src/components/FoodItem/FoodItem.jsx
+++ foodies/src/components/FoodItem/FoodItem.jsx
@@ -1,25 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { StoreContext } from "../../context/StoreContext";
 
-export const FoodItem = ({name,description,id,price,imageUrl}) => {
+export const FoodItem = ({ name, description, id, price, imageUrl }) => {
+  const { increaseQty, decreaseQty,quantities } = useContext(StoreContext);
   return (
-    <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4 d-flex justify-content-center"
-    style={{textDecoration:"none" }}>
+    <div
+      className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4 d-flex justify-content-center"
+      style={{ textDecoration: "none" }}
+    >
       <div
         className="card shadow-sm"
-        style={{ maxWidth: 320, borderRadius: "12px"}}
+        style={{ maxWidth: 320, borderRadius: "12px" }}
       >
         <Link to={`/food/${id}`}>
-        <img
-          src={
-            imageUrl ||
-            "https://images.unsplash.com/photo-1542291026-7eec264c27ff?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&w=1080"
-          }
-          className="card-img-top rounded-top"
-          alt={name || "Product Image"}
-          style={{ height: 200, objectFit: "cover" }}
-        />
-         </Link>
+          <img
+            src={
+              imageUrl ||
+              "https://images.unsplash.com/photo-1542291026-7eec264c27ff?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&w=1080"
+            }
+            className="card-img-top rounded-top"
+            alt={name || "Product Image"}
+            style={{ height: 200, objectFit: "cover" }}
+          />
+        </Link>
         <div className="card-body">
           <h5 className="card-title">{name || "Red Nike"}</h5>
           <p className="card-text text-truncate">
@@ -37,10 +41,24 @@ export const FoodItem = ({name,description,id,price,imageUrl}) => {
           </div>
         </div>
         <div className="card-footer d-flex justify-content-between bg-light border-0 pt-2">
-          <Link className="btn btn-primary btn-sm" to={`/food/${id}`}>View Food</Link>
-          <button className="btn btn-outline-primary btn-sm" >
-            <i className="bi bi-heart"></i>
-          </button>
+          <Link className="btn btn-primary btn-sm" to={`/food/${id}`}>
+            View Food
+          </Link>
+           {quantities[id]>0 ? (
+            <div>
+              <button className="btn btn-outline-primary btn-sm" onClick={()=>decreaseQty(id)}>
+         <i className="bi bi-plus-circle"></i>
+             </button>
+             <span className="fw-bold"> {quantities [id]}</span>
+             <button className="btn btn-success btn-sm" onClick={()=>increaseQty(id)}>
+              <i className="bi bi-plus-circle"></i>
+             </button>
+            </div>
+           ):(
+            <button className="btn btn-success btn-sm" onClick={()=>increaseQty(id)}>
+              <i className="bi bi-plus-circle"></i>
+             </button>
+           ) }
         </div>
       </div>
     </div>
--- foodies/src/context/StoreContext.jsx
+++ foodies/src/context/StoreContext.jsx
@@ -1,11 +1,23 @@
 import { createContext, useEffect, useState } from "react";
-import axios from "axios";
 import { fetchFoodList } from "./../service/foodService";
 
 export const StoreContext = createContext(null);
 
 export const StoreContextProvider = (props) => {
   const [foodList, setFoodList] = useState([]);
+
+  const [quantities, setQuantities] = useState({});
+
+  const increaseQty = (foodId) => {
+    setQuantities((prev) => ({ ...prev, [foodId]: (prev[foodId] || 0) + 1 }));
+  };
+
+  const decreaseQty = (foodId) => {
+    setQuantities((prev) => ({
+      ...prev,
+      [foodId]:   prev[foodId] > 0 ? prev[foodId] - 1 : 0,
+    }));
+  };
 
   useEffect(() => {
     async function loadData() {
@@ -17,6 +29,9 @@ export const StoreContextProvider = (props) => {
 
   const contextValue = {
     foodList,
+    increaseQty,
+    decreaseQty,
+    quantities
   };
 
   return (
