08dd76a7ecbb47f28e182af3c82d19d882c95896
orhan <orhantrkmn749@gmail.com>
2025-10-07 11:19:35 +0300

    login ve response için user entity repository service controller ve dtolar oluşturuldu

---
+++ foodis-api/src/main/java/com/example/foodis/api/io/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.example.foodis.api.io;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class AuthenticationResponse {
+
+    private String email;
+    private String token;
+}
---
+++ foodis-api/src/main/java/com/example/foodis/api/service/UserServiceImpl.java
@@ -0,0 +1,38 @@
+package com.example.foodis.api.service;
+
+import com.example.foodis.api.entity.UserEntity;
+import com.example.foodis.api.io.UserRequest;
+import com.example.foodis.api.io.UserResponse;
+import com.example.foodis.api.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserServiceImpl implements UserService{
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserResponse registerUser(UserRequest userRequest) {
+        UserEntity newUser = convertToEntity(userRequest);
+        userRepository.save(newUser);
+        return convertToResponse(newUser);
+    }
+
+    private UserEntity convertToEntity(UserRequest userRequest) {
+        return UserEntity.builder()
+                .email(userRequest.getEmail())
+                .password(userRequest.getPassword())
+                .name(userRequest.getName())
+                .build();
+    }
+
+    private UserResponse convertToResponse(UserEntity registeredUser) {
+        return UserResponse.builder()
+                .id(registeredUser.getId())
+                .email(registeredUser.getEmail())
+                .name(registeredUser.getName())
+                .build();
+    }
+}
