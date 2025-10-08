c619fe1261f1a611281b3b67cb014a89ed1d049f
orhan <orhantrkmn749@gmail.com>
2025-10-09 01:02:17 +0300

    sepete eklenen menü eşzamanlı cart veritabanından tutuldu.

--- foodies/src/context/StoreContext.jsx
+++ foodies/src/context/StoreContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useEffect, useState } from "react";
 import { fetchFoodList } from "./../service/foodService";
+import axios from "axios";
 
 export const StoreContext = createContext(null);
 
@@ -8,32 +9,43 @@ export const StoreContextProvider = (props) => {
 
   const [quantities, setQuantities] = useState({});
 
-  const [token,setToken] = useState("");
+  const [token, setToken] = useState("");
 
-  const increaseQty = (foodId) => {
+  const increaseQty = async (foodId) => {
     setQuantities((prev) => ({ ...prev, [foodId]: (prev[foodId] || 0) + 1 }));
+    await axios.post(
+      "http://localhost:8080/api/cart/add",
+      { foodId },
+      { headers: { Authorization: `Bearer ${token}` } }
+    );
   };
 
-  const decreaseQty = (foodId) => {
+  const decreaseQty = async(foodId) => {
     setQuantities((prev) => ({
       ...prev,
-      [foodId]:   prev[foodId] > 0 ? prev[foodId] - 1 : 0,
+      [foodId]: prev[foodId] > 0 ? prev[foodId] - 1 : 0,
     }));
+
+    await axios.post(
+      "http://localhost:8080/api/cart/remove",
+      { foodId },
+      { headers: { Authorization: `Bearer ${token}` } }
+    );
   };
 
   const removeFromCart = (foodId) => {
-    setQuantities((prevQuantities)=>{
-      const updateQuantities = {...prevQuantities};
-      delete updateQuantities [foodId];
+    setQuantities((prevQuantities) => {
+      const updateQuantities = { ...prevQuantities };
+      delete updateQuantities[foodId];
       return updateQuantities;
-    })
-  }
+    });
+  };
 
   useEffect(() => {
     async function loadData() {
       const data = await fetchFoodList();
       setFoodList(data);
-      if(localStorage.getItem("token")){
+      if (localStorage.getItem("token")) {
         setToken(localStorage.getItem("token"));
       }
     }
@@ -47,7 +59,7 @@ export const StoreContextProvider = (props) => {
     quantities,
     removeFromCart,
     token,
-    setToken
+    setToken,
   };
 
   return (
